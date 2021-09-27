a5ef27d051f05a621530a475fec5e7a47227ffb8
Abdulrahman <abudrm2020@gmail.com>
2021-09-27 13:10:29 +0300

    home responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,22 +10,22 @@ const Home = () => {
             <div className="mx-3">
 
                 <div className="row">
-                    <div className="col-6">
+                    <div className="col-sm-6 col-12">
                         <Product id={1} title="Lorem ipsum dolor sit amet consectetur" price={20.22} image="https://m.media-amazon.com/images/I/71WtwEvYDOS._AC_SY200_.jpg" rating={3}/>
                     </div>
-                    <div className="col-6">
+                    <div className="col-sm-6 col-12">
                     <Product id={6} title="Lorem ipsum dolor sit amet consectetur" price={20.22} image="https://m.media-amazon.com/images/I/61XYX1tzoTL._AC_SY200_.jpg" rating={5}/>
                     </div>
                 </div>
 
                 <div className="row mt-4">
-                    <div className="col-4">
+                    <div className="col-sm-4 col-12">
                     <Product id={2} title="Lorem ipsum dolor sit amet consectetur" price={20.22} image="https://m.media-amazon.com/images/I/61bwo1XYdES._AC_UL320_.jpg" rating={4}/>
                     </div>
-                    <div className="col-4">
+                    <div className="col-sm-4 col-12">
                     <Product id={3} title="Lorem ipsum dolor sit amet consectetur" price={20.22} image="https://m.media-amazon.com/images/I/81npNMXFqXL._AC_UL320_.jpg" rating={3}/>
                     </div>
-                    <div className="col-4">
+                    <div className="col-sm-4 col-12">
                     <Product id={4} title="Lorem ipsum dolor sit amet consectetur" price={20.22} image="https://m.media-amazon.com/images/I/71-yofISXUL._AC_UL320_.jpg" rating={1}/>
                     </div>
                 </div>
